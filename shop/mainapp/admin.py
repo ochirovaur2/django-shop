9230ccbcb883a9adfc8e9b473ee807687dc26f9f
Ochirov Aiur <ochirovaur2@mail.ru>
2021-02-22 10:21:42 +0700

    add image resolution restrictions

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,16 +1,33 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField, ModelForm
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from .models import *
+from PIL import Image
+
+class NotebookAdminForm(ModelForm):
+
+	MIN_RESOLUTION = (400, 400)
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['image'].help_text = f'Загружайте изображение с минимальным разрешением {self.MIN_RESOLUTION[0]}*{self.MIN_RESOLUTION[1]}'
 
 
-
+	def clean_image(self):
+		image = self.cleaned_data['image']
+		img = Image.open(image)
+		print(img.width, img.height)
+		min_height, min_width =  self.MIN_RESOLUTION
+		if img.height < min_height or img.width < min_height:
+			raise ValidationError('Минимальное разрешение изображения {}*{}, вы пытаетесь загрузить {}*{}'.format(*self.MIN_RESOLUTION, *(img.width, img.height,)))
+		return image
 
 class NotebookAdmin(admin.ModelAdmin):
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'category':
-            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+	form = NotebookAdminForm
+	def formfield_for_foreignkey(self, db_field, request, **kwargs):
+	    if db_field.name == 'category':
+	        return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+	    return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import include
 from . import views
 
 urlpatterns = [
-   	# path('', views.index, name = 'index')
+   	path('', views.index, name = 'index')
 	
 ]
